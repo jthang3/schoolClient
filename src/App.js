a683e5de523aa6987d56d14209b36b7719fa933b
jothang <jthang3@gmail.com>
2020-06-17 15:53:31 -0400

    6/17/2020

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 import Auth from "./auth/Auth"
-import Header from "./components/Navigation";
+import Header from "./Navigation";
 import {BrowserRouter as Router} from "react-router-dom";
 import AdvisorSignUpForm from "./auth/AdvisorSignUpForm";
 import StudentSignUpForm from "./components/Student/StudentSignUpForm";
