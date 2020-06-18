a2f0026d434d1302a40aebaeb257d8068268b930
jothang <jthang3@gmail.com>
2020-06-17 20:43:05 -0400

    working

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App() {
     const showReturn = ()=>{
     if(sessionToken === localStorage.getItem("token")){
       if(display === undefined){
+        console.log("THis is fucking working");
         return(
         <DisplayMe updateLog = {updateLog} updateDisplay = {updateDisplay} updateToken = {updateToke} sessionToken = {sessionToken}/>
         )
--- src/auth/Display.js
+++ src/auth/Display.js
@@ -2,6 +2,7 @@ import React from "react";
 import APIURL from "../helpers/environment";
 const DisplayMe = (props)=>{
     props.updateLog("LOGOUT")
+    console.log("THis mother fucker is worknig as well");
     const fetchMe = ()=>{
          //fetching happening too quickly for heroku to keep up. 
          fetch(`${APIURL}/advLog/advisorInfo`,{
